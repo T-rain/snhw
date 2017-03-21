9b7d17f0eac6b9cd211ac76f9590365596f68aec
travis Huang <travis965@gmail.com>
2017-03-22 00:35:22 +0800

    hw2-addRedbyjs

--- docs/hw2/javascripts/hw2.js
+++ docs/hw2/javascripts/hw2.js
@@ -2,46 +2,19 @@ document.getElementById('scroll-top').onclick = function(){
     window.scrollTo(0,0);
 }
 
+var needToChangeTagArray=['p','h3','h1'];
+needToChangeTagArray.forEach(function(element) {
+    var divpbgHTMLcollectionLength =  document.getElementsByTagName(element).length;
+    for (var i = 0; i < divpbgHTMLcollectionLength; i++) {
 
-
-
-document.getElementsByClassName("pbgcolor")[0].onmouseover = function(){
-  document.getElementsByClassName("pbgcolor")[0].className = "pbgcolor pbgcolorpink";
-}
-
-document.getElementsByClassName("pbgcolor")[1].onmouseover = function(){
-  document.getElementsByClassName("pbgcolor")[1].className = "pbgcolor pbgcolorpink";
-}
-
-document.getElementsByClassName("pbgcolor")[2].onmouseover = function(){
-  document.getElementsByClassName("pbgcolor")[2].className = "pbgcolor pbgcolorpink";
-}
-
-document.getElementsByClassName("pbgcolor")[0].onmouseout = function(){
-  document.getElementsByClassName("pbgcolor")[0].className = "pbgcolor";
-}
-
-document.getElementsByClassName("pbgcolor")[1].onmouseout = function(){
-  document.getElementsByClassName("pbgcolor")[1].className = "pbgcolor";
-}
-
-document.getElementsByClassName("pbgcolor")[2].onmouseout = function(){
-  document.getElementsByClassName("pbgcolor")[2].className = "pbgcolor";
-}
-
-//var needToChangeTagArray=['p','h3','h1'];
-// needToChangeTagArray.forEach(function(element) {
-//     var divpbgHTMLcollectionLength =  document.getElementsByTagName(element).length;
-//     for (var i = 0; i < divpbgHTMLcollectionLength; i++) {
-//
-//         document.getElementsByTagName(element)[i].onmouseover = function(ele){
-//             ele.target.className = "pbgcolorpink";
-//         }
-//         document.getElementsByTagName(element)[i].onmouseout = function(ele){
-//             ele.target.className = " ";
-//         }
-//     }
-// });
+        document.getElementsByTagName(element)[i].onmouseover = function(ele){
+            ele.target.className = "pbgcolorpink";
+        }
+        document.getElementsByTagName(element)[i].onmouseout = function(ele){
+            ele.target.className = " ";
+        }
+    }
+});
 
 var needToChangeIDSize=['x-small','medium','x-large'];
 
