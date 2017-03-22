cde9eb0cfa326e2154cca27d5174cbc43d13514e
travis Huang <travis965@gmail.com>
2017-03-22 14:22:45 +0800

    fixHw2jsToBetterWay

--- docs/hw2/javascripts/hw2.js
+++ docs/hw2/javascripts/hw2.js
@@ -1,24 +1,34 @@
+//scrollToTop
 document.getElementById('scroll-top').onclick = function(){
     window.scrollTo(0,0);
 }
 
-var needToChangeTagArray=['p','h3','h1'];
-needToChangeTagArray.forEach(function(element) {
-    var divpbgHTMLcollectionLength =  document.getElementsByTagName(element).length;
-    for (var i = 0; i < divpbgHTMLcollectionLength; i++) {
+//add background-color on reading paragraph
+var getEleAndIter = function(cb){
+  var needToChangeTagArray=['p','h3','h1'];
+  needToChangeTagArray.forEach(function(element) {
+      var divpbgHTMLcollectionLength =  document.getElementsByTagName(element).length;
+      for (var i = 0; i < divpbgHTMLcollectionLength; i++) {
+          cb(element,i);
+      }
+  });
 
-        document.getElementsByTagName(element)[i].onmouseover = function(ele){
-            ele.target.className += " pbgcolorpink";
-        }
-        document.getElementsByTagName(element)[i].onmouseout = function(ele){
-            var classNameString = ele.target.className;
-            var index = ele.target.className.indexOf("pbgcolorpink");
-            var changeName = classNameString.substring(0,index);
-            ele.target.className = changeName;
-        }
+}
+
+getEleAndIter(function(element,i){
+    document.getElementsByTagName(element)[i].onmouseover = function(){
+      document.getElementsByTagName(element)[i].className += " pbgcolorpink";
+    }
+
+    document.getElementsByTagName(element)[i].onmouseout = function(){
+        var classNameString = document.getElementsByTagName(element)[i].className;
+        var index = classNameString.indexOf("pbgcolorpink");
+        var changeName = classNameString.substring(0,index);
+        document.getElementsByTagName(element)[i].className = changeName;
     }
 });
 
+//change font-size
 var needToChangeIDSize=['x-small','medium','x-large'];
 
 needToChangeIDSize.forEach(function(ele){
